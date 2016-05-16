28b35f89f08dc1767509f6bc383234a2762b45f1
wayne <278972894@qq.com>
2016-05-16 21:47:16 +0800

    test

---
+++ time_test.py
@@ -0,0 +1,18 @@
+#########################################################################
+#-*- coding:utf-8 -*-
+# File Name: time_test.py
+#########################################################################
+#!/bin/python
+from datetime import datetime
+from datetime import timedelta
+t1 = datetime.now()
+minute_num = t1.minute
+if minute_num >= 30:
+    delta = minute_num - 30
+else:
+    delta = minute_num
+
+t2 = t1 - timedelta(minutes=delta)
+print t2
+
+
