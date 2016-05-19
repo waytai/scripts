fafcb0910eb0f28fa37b1dae5a420d715346900b
wayne <278972894@qq.com>
2016-05-19 16:04:27 +0800

    git clone

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: clone.sh
+# Author: wayne
+# mail: @163.com
+# Created Time: 2016年04月26日 星期二 16时58分23秒
+#########################################################################
+#!/bin/bash
+#git clone git@github.com:heyfluke/aliyun_mqs_python_client.git
+git clone git@github.com:waytai/scripts.git
