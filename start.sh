c75fc55e8a4c0593bab3b22c45fce37587f657ba
wayne <278972894@qq.com>
2016-05-19 13:56:25 +0800

    start django

---
+++ start.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: env.sh
+# Author: wayne
+# mail: @163.com
+# Created Time: 2016年05月08日 星期日 09时27分23秒
+#########################################################################
+#!/bin/bash
+
+cd laas_back
+source env.sh
+python manage.py runserver
