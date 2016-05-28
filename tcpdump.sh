2db647d10801a74eb0fce8fcfae48cba0729e9e7
wayne <278972894@qq.com>
2016-05-28 19:56:15 +0800

    pcap

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,1 +1,10 @@
-sudo tcpdump -i wlan0 host 192.168.1.106 -w liu.cap
+#保存为cap文件格式
+#tcpdump -i wlan0 host 192.168.1.106 -w liu.cap
+
+#保存为pcap文件格式
+tcpdump -w test.pcap -i wlan0
+#读取pcap文件格式
+tcpdump -nnr test.pcap
+
+
+
