b0dbd8131d8ad89f2d497551e790f649dd010f50
wayne <278972894@qq.com>
2016-09-08 15:07:54 +0800

    pg shell

---
+++ pg.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: pg.sh
+# Author: wayne
+# mail: @163.com
+# Created Time: 2016年08月31日 星期三 16时11分32秒
+#########################################################################
+#!/bin/bash
+
+#psql --host=host --port=5432 --username=postgres --password --dbname=dbname
+#psql --host=host --port=5432 --username=rdsadmin --password 
+#psql --host=localhost --port=5432 --username=postgres --password --dbname=iot
+
+#nc -zv host 5432
+
+#pg_restore -h host -p 5432 -U postgres  -d fogcloud_test_20  example.sql
+
+#psql -h host -p 5432 -U postgres -d example -f example.sql
+
+psql --host=host --port=5432 --username=postgres --password 
