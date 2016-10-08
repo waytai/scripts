c8b609627a211687e99107217ee020898b3b82d6
wayne <278972894@qq.com>
2016-10-08 15:39:15 +0800

    hex to string

--- hex_to_string.py
+++ hex_to_string.py
@@ -11,3 +11,18 @@ print new_key
 print a.decode('hex')
 n = new_key.replace('0x','')
 print n.decode('hex')
+
+
+test = 'abc'
+str_to_hex =''.join(hex(ord(s)) for s in test)
+print str_to_hex 
+
+#hex_to_str = str_to_hex.split('0x')
+#print hex_to_str
+#for element in hex_to_str:
+#    if len(element):
+#        print chr(int(element,16))
+
+hex_to_str = ''.join(chr(int(element,16))for element in str_to_hex.split('0x') if len(element))
+print hex_to_str
+
