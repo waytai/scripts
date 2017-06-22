5da8d8b24aa8e45d67f0f0a85a86206cb6aa42ac
wayne <278972894@qq.com>
2017-06-22 17:16:06 +0800

    test

--- twistedtcp.py
+++ twistedtcp.py
@@ -1,4 +1,5 @@
 import os
+import pdb
 if os.name!='nt':
     from twisted.internet import epollreactor
     epollreactor.install()    
@@ -7,15 +8,24 @@ else:
     iocpreactor.install()
 from twisted.internet.protocol import Factory,Protocol
 from twisted.internet import reactor
+client_socks = []
+
 class gameSocket(Protocol):
+    def __init__(self):
+        print "777"
+
     def connectionMade(self):
         print 'New Client'
+        #pdb.set_trace()
+        client_socks.append(self.transport.socket)
 
     def connectionLost(self,reason):
         print 'Lost Client'
-    
+
     def dataReceived(self, data):
         print 'Get data:' + str(data)
+
+
         server_ip_port = self.transport.server.getHost()
         server_ip = server_ip_port.host
         server_port = server_ip_port.port
@@ -24,6 +34,8 @@ class gameSocket(Protocol):
         client_ip_port = self.transport.client
         client_ip = client_ip_port[0]
         client_port = str(client_ip_port[1])
+
+        print "%"*10, client_socks 
 
         print "="*20, client_ip, client_port
         self.transport.write('bingo!i got your msg:'+ str(data))
